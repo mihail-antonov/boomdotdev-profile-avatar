e332222393f0c67b1b3f4828fe1fe3e06630b20a
Mihail Antonov <mihail.d.antonov@gmail.com>
2023-01-17 22:14:58 +0200

    on click set scale 2.0 on image logic implementation

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
 
-  const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
-    alert("💣");
+  let image = document.querySelector('.image');
+
+  image.addEventListener('click', () => {
+
+    image.style.transform = "scale(2.0)";
   });
 });
